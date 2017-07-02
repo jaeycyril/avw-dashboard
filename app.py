935e026a212cbd9fcaa035b38f460d4b79fdd1d0
cyril ukwajiunor <jaeycyril@gmail.com>
2017-07-03 00:26:06 +0100

    Refactor monolithic code into functional units

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/2/')
+def index2():
+	return render_template('index2.html')
+
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True )
