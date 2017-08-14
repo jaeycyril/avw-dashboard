04d6e619f1412f6629f598922022df0a2e9fdc81
cyril ukwajiunor <jaeycyril@gmail.com>
2017-08-14 04:29:33 +0100

    Add context and documentation to ease evaluation

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/help')
+def help():
+	return render_template('help.html')
+
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True )
